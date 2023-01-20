7ddd0d008df8a6ab8733f0ce3ad577413bc3bd55
shaheer34mts <muhammad_shaheer_ahmed@outlook.com>
2023-01-20 23:50:58 +0100

    [feat] - add animation

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function togglePublication(event) {
         publicationContent.style.maxHeight = publicationContent.scrollHeight + "px";
     }
 }
-particlesJS.load('particles-js', 'path/to/particles.json', function() {
+particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
